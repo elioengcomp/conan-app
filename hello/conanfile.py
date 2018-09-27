1c2e54a36f72bdc8e36776307565bb0d4fce1065
Elio Marcolino <eliom@jfrog.com>
2018-09-27 10:57:59 -0700

    Changed version

--- hello/conanfile.py
+++ hello/conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake
 
 class HelloConan(ConanFile):
     name = "Hello"
-    version = "0.1"
+    version = "0.2"
     license = "<Put the package license here>"
     url = "<Package recipe repository url here, for issues about the package>"
     description = "<Description of Hello here>"
